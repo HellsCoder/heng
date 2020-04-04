7345663a1bf8a7511dffc8586f5aed5b70feea08
HellsCoder <marat.zhurkin@gmail.com>
2020-04-04 14:07:59 +0200

    default routes

---
+++ app/routes/routes.js
@@ -0,0 +1,11 @@
+
+const routes = [
+    {
+        link: '/',
+        callback: () => {
+          console.info("Эта функция будет выполнена когда пользователь перейдет по адресу /");
+        }
+    }
+];
+
+module.exports = routes;
