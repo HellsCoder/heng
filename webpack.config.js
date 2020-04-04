a6ab5218781ad4a1ebc305f63e7b13f2d3df0318
HellsCoder <marat.zhurkin@gmail.com>
2020-04-04 13:41:25 +0200

    add version system in bundles
    for disable bundles browser cacheing add version system

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,56 @@
     Это конфиг вебпака, его лучше не трогать
 */
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   context: __dirname,
+    //heng entry file
   entry: "./heng/app.js",
   output: {
     path: __dirname + "/build",
-    filename: "bundle.js"
+    filename: "bundle.[hash].js"
   },
-  module:{
-    rules:[
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({}), 
+      new OptimizeCSSAssetsPlugin({}),
     ]
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        minify: { collapseWhitespace: true },
+        template: 'heng/index.html',  
+        filename: './../index.html'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: 'app.[hash].css'
+    }),
+    new CleanWebpackPlugin()
+  ]
 }
