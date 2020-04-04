5065fea61b28d5a1761cd720017e50f700dd70e9
HellsCoder <marat.zhurkin@gmail.com>
2020-04-04 14:09:24 +0200

    default routes

---
+++ app/routes/routes_dom.js
@@ -0,0 +1,11 @@
+const routes_dom = [
+    {
+        selector: '.button',
+        type: 'click',
+        callback: () => {
+          /* Функция будет вызвана при нажатии на объект с классом .button */
+        }
+    }
+];
+
+module.exports = routes_dom;
